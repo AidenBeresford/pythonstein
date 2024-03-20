b57282498c3403fa295b14f12261d15e8ec98ea7
AidenBeresford <aidenberes@gmail.com>
2024-03-20 15:02:52 -0500

    Fixed texture offset

--- renderer.py
+++ renderer.py
@@ -31,6 +31,9 @@ class Renderer:
         line = 0
         for t in np.linspace(self.dir.angle() - math.radians(FOV/2), self.dir.angle() + math.radians(FOV/2), WINDOW_RESOLUTION[0]):
             target = Vector2(RAYLENGTH*math.cos(t) + self.pos.x, -RAYLENGTH*math.sin(t) + self.pos.y)
+    
+            if line in range(10):
+                print(target.tuplify())
 
             # the following algorithm is supposed to be dda but it's like the fucked up twisted version from my deep psyche
             # viewer discretion advised >:)
@@ -81,9 +84,8 @@ class Renderer:
 
             # line height rendering
 
-            pdist = sdx - ddx if side == 0 else sdy - ddy
+            pdist = sdx - ddx if side == 0 else sdy - ddy 
             pdist *= math.cos(self.dir.angle() - t)        
-    
             pdist = 0.00001 if pdist == 0 else pdist
 
             lineh = WINDOW_RESOLUTION[1] / pdist * (TILE_SIZE/4)
@@ -95,21 +97,21 @@ class Renderer:
             bottom = lineh if bottom >= lineh else lineh
  
             # texture rendering
- 
-            wall = self.pos.y + pdist * target.y if side == 0 else self.pos.x + pdist * target.x
 
+            wall = -self.pos.y + pdist * math.sin(t) if side == 0 else self.pos.x + pdist * math.cos(t)
+            wall /= TILE_SIZE
             wall -= math.floor(wall)
 
             tex = self.textures[self.map[mapy][mapx]-1]
 
-            #texw, texh = tex.size
+            texw, texh = tex.get_width(), tex.get_height()
             
-            texx = wall * 64 #* texw       
+            texx = wall * texw       
           
-            column = tex.subsurface(int(texx), 0, 1, 64)
+            column = tex.subsurface(texx, 0, 1, 64)
             column = pygame.transform.scale(column, (1, lineh*2))
 
-            self.screen.blit(column, (line, int(256/2 - lineh/2)))
+            self.screen.blit(column, (line, (256/2 - lineh/2)))
             
             line += 1
 
